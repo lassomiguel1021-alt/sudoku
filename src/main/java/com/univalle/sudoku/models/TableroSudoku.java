b5339e1db52f5e4b13071175c50cb20cf2ec7a63
Miguel Angel Lasso Troncoso <lassomiguel1021@gmail.com>
2026-06-15 22:59:23 -0500

    Clase TableroSudoku completada con sus métodos base

--- src/main/java/com/univalle/sudoku/models/TableroSudoku.java
+++ src/main/java/com/univalle/sudoku/models/TableroSudoku.java
@@ -1,4 +1,40 @@
 package com.univalle.sudoku.models;
 
 public class TableroSudoku {
+    private int[][] tablero = new int[6][6];
+
+    public int getNumero(int fila, int columna) {
+        return tablero[fila][columna];
+    }
+
+    public void setNumero(int fila, int columna, int numero) {
+        tablero[fila][columna] = numero;
+    }
+
+    public boolean estaVacio(int fila, int columna) {
+        return tablero[fila][columna] == 0;
+    }
+
+    public void imprimirTablero() {
+        for (int fila = 0; fila < 6; fila++){
+            for (int columna = 0; columna < 6; columna++){
+                System.out.print(tablero[fila][columna] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int [][] clonarTablero() {
+        int[][] copia = new int [6][6];
+        for (int fila = 0; fila < 6; fila++){
+            for (int columna = 0; columna < 6; columna++){
+                copia[fila][columna] = tablero[fila][columna];
+            }
+        }
+        return copia;
+    }
+
+    public int[][] getTablero() {
+        return tablero;
+    }
 }
