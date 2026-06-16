5936f5a0beedf961bc2972df80167324245ea59a
Miguel Angel Lasso Troncoso <lassomiguel1021@gmail.com>
2026-06-15 23:32:59 -0500

    Metodos revisarFila y revisarColumna agregados

--- src/main/java/com/univalle/sudoku/models/ValidadorSudoku.java
+++ src/main/java/com/univalle/sudoku/models/ValidadorSudoku.java
@@ -1,4 +1,21 @@
 package com.univalle.sudoku.models;
 
 public class ValidadorSudoku {
+    public boolean revisarFila(int[][] tablero, int fila,int numero){
+        for (int columna=0; columna < 6; columna++){
+            if (tablero[fila][columna] == numero){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean revisarColumna(int[][] tablero, int columna, int numero){
+        for (int fila=0; fila < 6; fila++){
+            if (tablero[fila][columna] == numero){
+                return false;
+            }
+        }
+        return true;
+    }
 }
