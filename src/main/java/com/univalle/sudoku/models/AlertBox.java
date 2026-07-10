cecdd0f546b833e6f5b771e280d431a15e4bce41
Miguel Angel Lasso Troncoso <lassomiguel1021@gmail.com>
2026-07-09 20:27:15 -0500

    IAlertBox y AlertBox completadas para manejo de alertas

--- src/main/java/com/univalle/sudoku/models/AlertBox.java
+++ src/main/java/com/univalle/sudoku/models/AlertBox.java
@@ -1,4 +1,15 @@
 package com.univalle.sudoku.models;
 
-public class AlertBox {
+import javafx.scene.control.Alert;
+
+public class AlertBox implements IAlertBox {
+
+    @Override
+    public void mostrarAlerta(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
 }
--- src/main/java/com/univalle/sudoku/models/IAlertBox.java
+++ src/main/java/com/univalle/sudoku/models/IAlertBox.java
@@ -1,4 +1,5 @@
 package com.univalle.sudoku.models;
 
 public interface IAlertBox {
+    void mostrarAlerta(String titulo, String mensaje);
 }
